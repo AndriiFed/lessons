86ca21e4ae9ac242d3db80b3dd7cde57fe1dd1a4
Dmitriy Butakov <dmitry404@gmail.com>
2016-10-10 23:12:57 +0300

    * expressions - assignments, method invocation, modulus operator in Java * if/else control flow

--- lesson2/src/some/program/FirstProgram.java
+++ lesson2/src/some/program/FirstProgram.java
@@ -2,6 +2,12 @@ package some.program;
 
 class FirstProgram {
     public static void main(String[] args) {
-      
+      int i = 10;
+
+      if (i % 2 == 0) {
+          System.out.println("Even");
+      } else {
+          System.out.println("Odd");
+      }
     }
 }
