47b072848fe17a68f31e8de3e8c801d64c7fddd6
Dmitriy Butakov <dmitry404@gmail.com>
2016-10-10 23:47:59 +0300

    * Primitives and objects in Java * Default values (all local variables must be initialized) and sizes * Arrays and Strings

--- lesson2/src/some/program/FirstProgram.java
+++ lesson2/src/some/program/FirstProgram.java
@@ -2,16 +2,25 @@ package some.program;
 
 class FirstProgram {
     public static void main(String[] args) {
-      for (int i = 1; i <= 5; i++) {
-          System.out.print(i);
-          System.out.print(" is even? ");
-          System.out.println(isEven(i));
-      }
+      short shortVar = 0; // 2 bytes
+      int integerVar = 0; // 4 bytes
+      long longValue = 0l; // 8 bytes
 
-      // You can find the operator precedence table there https://docs.oracle.com/javase/tutorial/java/nutsandbolts/operators.html
-      System.out.println(2 + 2 * 2);
-      System.out.println((2 + 2) * 2);
+      byte byteVar = 0; // 1 byte
 
+      float floatVar = 0.0f; // 4 bytes
+      double doubleVar = 0.0d; // 8 bytes
+
+      char charVar = '\u0000'; // 2 byte - unicode in UTF-16
+
+      boolean booleanVar = false; // not defined by the specification. Depends on JVM implementation
+
+      Object obj = null;
+
+      String[] arr = {"I'm a part of array", "I'm the same egg here"};
+
+      String str = "I'm a string";
+      String str2 = new String("Hello. I'm a string as well as my sister above");
     }
 
     public static boolean isEven(int i) {
