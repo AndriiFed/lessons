23a6033b3a3aba51971ee697449d3f96bd26538c
Dmitriy Butakov <dmitry404@gmail.com>
2016-10-10 23:40:12 +0300

    * return statement as a way to return method's invocation result * boolean (true or false)

--- lesson2/src/some/program/FirstProgram.java
+++ lesson2/src/some/program/FirstProgram.java
@@ -2,21 +2,18 @@ package some.program;
 
 class FirstProgram {
     public static void main(String[] args) {
-      int i = 0;
-      while (true) { // infinite loop
-          if (i != 5) return;
+      for (int i = 1; i <= 5; i++) {
+          System.out.print(i);
+          System.out.print(" is even? ");
+          System.out.println(isEven(i));
       }
-      // In case of "return" above the method invokation below would cause compilation error: unreachable statement
-      //System.out.println("Elvis has left the building");
     }
 
-    public static void isEven(int i) {
-      System.out.print(i);
-
+    public static boolean isEven(int i) {
       if (i % 2 == 0) {
-          System.out.println(" is even");
+          return true;
       } else {
-          System.out.println(" is odd");
+          return false;
       }
     }
 }
