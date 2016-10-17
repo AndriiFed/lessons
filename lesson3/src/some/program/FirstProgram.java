b90ce95b7a516f1fd8a2e9379e6c4ae6b46573f8
Dmitriy Butakov <dmitry404@gmail.com>
2016-10-17 19:30:50 +0300

    First

---
+++ lesson3/src/some/program/FirstProgram.java
@@ -0,0 +1,10 @@
+package some.program;
+
+class FirstProgram {
+    public static void main(String[] args) {
+    }
+
+    public static boolean isEven(int i) {
+      return (i % 2 == 0);
+    }
+}
