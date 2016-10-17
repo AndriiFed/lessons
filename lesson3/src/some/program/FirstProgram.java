ae96c9c518fe9e3e74ecdee34e887d78ee6de359
Dmitriy Butakov <dmitry404@gmail.com>
2016-10-17 19:45:11 +0300

    Student ans Student2 variables

--- lesson3/src/some/program/FirstProgram.java
+++ lesson3/src/some/program/FirstProgram.java
@@ -2,9 +2,34 @@ package some.program;
 
 class FirstProgram {
     public static void main(String[] args) {
+      Object obj = new Object();
+      String str = "hello";
+
+      Student student = new Student();
+      student.name = "John Doe";
+      student.age = 12;
+
+      Student student2 = new Student();
+      student2.name = "Johnathan Doelsky";
+      student2.age = 92;
+
+
+      System.out.println(student);
+      System.out.println(student2);
     }
 
-    public static boolean isEven(int i) {
-      return (i % 2 == 0);
-    }
+
+}
+
+class Student {
+  public String name;
+  public int age;
+
+  public String toString() {
+    return "I'm a student. My name is " + name + ". I'm " + age + " years old." ;
+  }
+
+  public void foo() {
+    name = "";
+  }
 }
