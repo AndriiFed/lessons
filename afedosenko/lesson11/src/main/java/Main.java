0f63a418ec65a7baf27bb12577eec015b8117cad
Andrii Fedosenko <oramazd@gmail.com>
2016-11-10 19:49:25 +0200

    lesson11, p2

--- afedosenko/lesson11/src/main/java/Main.java
+++ afedosenko/lesson11/src/main/java/Main.java
@@ -1,9 +1,12 @@
+import shape.example1.Shape;
 
 public class Main {
   public static void main(String ... args) {
     Animal animal = new Animal();
     Dog dog = new Dog();
     Cat cat = new Cat();
+
+    Cat catNull = null;
 
     if (animal instanceof Animal) {
       System.out.println("Animal");
@@ -15,7 +18,14 @@ public class Main {
       System.out.println("Cat");
     }
 
-    
+    if (catNull instanceof Cat) {
+      System.out.println("A");
+    }
 
+    Shape shape = new Shape(2, 4, 5);
+    System.out.format("%nAREAS OF SHAPES:%n");
+    System.out.format("%.2f %n", shape.calculateArea(Shape.ShapeType.SQUARE));
+    System.out.format("%.2f %n", shape.calculateArea(Shape.ShapeType.RECTANGLE));
+    System.out.format("%.2f %n", shape.calculateArea(Shape.ShapeType.CIRCLE));
   }
 }
---
+++ afedosenko/lesson11/src/main/java/shape/example1/Shape.java
@@ -0,0 +1,30 @@
+package shape.example1;
+
+public class Shape {
+  private double width;
+  private double height;
+  private double radius;
+
+  public static enum ShapeType {
+    SQUARE, RECTANGLE, CIRCLE
+  }
+
+  //public static int SQUARE = 1;
+  public Shape(double width, double height, double radius) {
+    this.width = width;
+    this.height = height;
+    this.radius = radius;
+  }
+  public double calculateArea(ShapeType shape) {
+    double area = 0.0;
+    switch (shape) {
+      case SQUARE: area = width * width;
+        break;
+      case RECTANGLE: area = width * height;
+        break;
+      case CIRCLE: area = Math.PI * radius * radius;
+    }
+    return area;
+  }
+
+}
