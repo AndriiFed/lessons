4edd57189e2951a83f4638902643ab047c542ff7
Andrii Fedosenko <oramazd@gmail.com>
2016-12-05 18:45:31 +0200

    add lesson18

---
+++ afedosenko/lesson18/src/test/java/PQTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Ignore;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+
+
+public class PQTest {
+
+  @Test
+  public void test1() {
+
+    System.out.println("Test out");
+
+  }
+
+
+}
+
