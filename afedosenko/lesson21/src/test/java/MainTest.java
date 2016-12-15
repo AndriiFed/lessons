1ad938d489876e120d487586aa2ebb798cb22381
Andrii Fedosenko <oramazd@gmail.com>
2016-12-15 19:05:47 +0200

    Add lesson21

---
+++ afedosenko/lesson21/src/test/java/MainTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Ignore;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class MainTest {
+
+  @Test
+  public void name() throws Exception {
+
+
+    assertThat(0, is(0));
+
+
+  }
+
+}
