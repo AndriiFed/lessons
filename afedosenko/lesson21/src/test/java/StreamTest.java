4724566eb861c69a17e7c3a80f7ef6ca634a64c1
Andrii Fedosenko <oramazd@gmail.com>
2016-12-15 20:12:54 +0200

    lesson21. part1

---
+++ afedosenko/lesson21/src/test/java/StreamTest.java
@@ -0,0 +1,13 @@
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Ignore;
+
+public class StreamTest {
+
+  @Test
+  public void test1() throws Exception {
+
+
+  }
+}
